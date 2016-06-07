5d9f2a7a567f34ae9d1c1e3d0df8c05d39483718
Tom Scanlan <tompscanlan@gmail.com>
2016-06-07 12:08:56 -0400

    move scripted stuff to chef

--- provision/chef/cookbooks/packerd/recipes/default.rb
+++ provision/chef/cookbooks/packerd/recipes/default.rb
@@ -1,3 +1,30 @@
+deb = '/tmp/chefdk.deb'
+
+remote_file deb do
+  source 'https://omnitruck.chef.io/stable/chefdk/download?p=ubuntu&m=x86_64&pv=14.04&v=latest'
+end
+
+dpkg_package "chefdk" do
+  source "/tmp/chefdk.deb"
+  action :install
+end
+
+remote_file deb do
+  action :delete
+end
+
+directory '/root/.berkshelf' do
+  owner 'root'
+  mode '0755'
+  action :create
+end
+
+file "/root/.berkshelf/config.json" do
+  owner 'root'
+  mode '0755'
+  content '{"ssl": { "verify": false }}'
+  action :create
+end
 
 directory '/etc/supervisor' do
   owner 'root'
